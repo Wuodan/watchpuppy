0ab6bd40da36f36e926fa796573fa38270d4f047
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2024-10-24 19:24:29 +0200

    wip

--- docker-build-run.sh
+++ docker-build-run.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
+# for debugging
+# builds the image and runs a container indefinitely to exec into
+# git-bash and docker volumes can be tricky to handle because of path translation
+
 set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# SLEUTHKIT_VERSION=sleuthkit-4.12.1
-SLEUTHKIT_VERSION=ff3c3c7fb68394a4406e275b578b5345ee54a708
-
-IMG_NAME=sleuthkit:${SLEUTHKIT_VERSION}
-CONTAINER_NAME=sleuthkit-${SLEUTHKIT_VERSION}
+IMG_NAME=wuodan/watchpuppy:latest
+CONTAINER_NAME=watchpuppy
 
 docker build \
   --file Dockerfile \
-  --build-arg SLEUTHKIT_VERSION=$SLEUTHKIT_VERSION \
   -t $IMG_NAME \
   "$SCRIPT_DIR"
 
@@ -21,10 +21,10 @@ echo "Docker image: $IMG_NAME"
 # MSYS_NO_PATHCONV prevents path fubar by git-bash on windows
 MSYS_NO_PATHCONV=1 docker run -d \
   -v "$SCRIPT_DIR/input/:/data/input:ro" \
-  -v "$SCRIPT_DIR/output/:/data/output:rw" \
-  -v "$SCRIPT_DIR/sleuthkit_script.py:/data/sleuthkit_script.py:ro" \
   --name "$CONTAINER_NAME" \
   $IMG_NAME \
   tail -f /dev/null
 
-echo "Docker container '$CONTAINER_NAME' running"
+echo "Docker container '$CONTAINER_NAME' running."
+echo "Run this for shell access:"
+printf '\tdocker exec -it %s bash\n' "$CONTAINER_NAME"
